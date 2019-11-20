701a4c2682305f4d29731c94ae11b59a2973d46c
janealdash <janarka.k@gmail.com>
2019-11-20 14:53:20 +0600

    janara

---
+++ task2.10.py
@@ -0,0 +1,10 @@
+a = "Zhanara is a good girl and she likes coding"
+print (a [2]) 
+print (a [-2])
+print (a [0:5])
+print (a [:-2])
+print (a [1::2])
+print (a [::2])
+print (a [::-1])
+print (a [-2::-2])
+print(len(a))
